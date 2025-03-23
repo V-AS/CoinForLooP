b141222faca5ccce2b20046063d97f1cd7c6e2cd
ZHI PI <paulbigba@gmail.com>
2025-03-23 04:44:34 -0600

    update api to update user income with month and year

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,11 +9,27 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    income = Column(Float, default=0.0)
     
     # Relationships
     transactions = relationship("Transaction", back_populates="user")
     goals = relationship("Goal", back_populates="user")
+    incomes = relationship("UserIncome", back_populates="user") 
+
+class UserIncome(Base):
+    __tablename__ = "user_incomes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    year = Column(Integer)
+    month = Column(Integer)
+    income = Column(Float, default=0.0)
+    
+    __table_args__ = (
+        UniqueConstraint("user_id", "year", "month", name="unique_user_income_per_month"),
+    )
+    
+    # Relationships
+    user = relationship("User", back_populates="incomes")
 
 class Transaction(Base):
     __tablename__ = "transactions"
